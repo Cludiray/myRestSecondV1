a92908f5ddec8c1670f4877add37a1c413ff665d
cludiray <cludiray@gmail.com>
2023-01-19 10:54:22 +0300

    Error for manual mvn

--- src/main/java/com/example/restservice/controllers/DateController.java
+++ src/main/java/com/example/restservice/controllers/DateController.java
@@ -2,7 +2,7 @@ package com.example.restservice.controllers;
 
 import com.example.restservice.service.DateService;
 import com.example.restservice.model.MyDate;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
